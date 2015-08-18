8e20980c9d3148a47576652fd00ef78323cdf297
drillDash <azimov1990@yandex.ru>
2015-08-18 15:23:27 +0300

    Commit #2 Adding ".gitignore"-file

--- src/org/item2/TestComputerBuild.java
+++ src/org/item2/TestComputerBuild.java
@@ -9,7 +9,7 @@ import java.util.Map;
 
 public class TestComputerBuild {
     public static void main(String[] args) {
-        Computer Comp = new Computer.Builder(1, 3).hasDiscreteGpu(Boolean.TRUE).cpuName("AMD").build();
+        Computer Comp = new Computer.Builder(1, 3).hasDiscreteGpu(Boolean.TRUE).cpuName("AMD").gpuName("Radeon 7860").build();
 
 
 //        Class<?> c = Comp.getClass();
