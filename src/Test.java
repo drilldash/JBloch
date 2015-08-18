b72d25a5203f08620682c3f6fdea716a1dbd9040
drillDash <azimov1990@yandex.ru>
2015-08-18 12:43:01 +0300

    initial commit

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+/**
+ * Created by verlorener on 17.08.15.
+ */
+public class Test {
+    public int value;
+    public static int staticValue;
+    public int getValue() {
+        return ++value;
+    }
+
+    public static int getStaticValue() {
+        return ++staticValue;
+    }
+}
+
